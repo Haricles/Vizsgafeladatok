16890f52568c51e61bc4f6772470eea263616c48
Haricles <https://github.com/Haricles/>
2023-01-20 06:41:15 +0100

    morze dekodolo

--- VIzsgafeladat_08/main.py
+++ VIzsgafeladat_08/main.py
@@ -1,36 +1,45 @@
+def sztring_morzekodda(sztring):
+    uj_sztring = ""
+    for elem in sztring:
+        if elem.upper() == " ":
+            uj_sztring += "       "
+        for key, value in morzeabc.items():
+            if elem.upper() == key:
+                uj_sztring += value + " "
+    print(f"A megadott sztring morze kódja: {uj_sztring}")
+
+def morze_dekodolo(kod):
+    uj_sztring = ""
+    kod = kod.split()
+    for karakter in kod:
+        for key, value in morzeabc.items():
+            if karakter == value:
+                uj_sztring += key + " "
+    print(uj_sztring)
+
+def morze_dekodolo_2(morze_szoveg):
+    szoveg = ""
+    morze_szoveg = morze_szoveg.replace("   ", ";")
+    reszek = morze_szoveg.split(";")
+    for karakter in reszek:
+        betuk = karakter.split(" ")
+        for betu in betuk:
+            szoveg+=" "
+            for key, value in morzeabc.items():
+                if betu == value:
+                    szoveg += key
+    print(szoveg)
+
 morzeabc={}
-with open("morzeabc.txt","r") as file:
+with open("morzeabc.txt","r",encoding="ISO-8859-1") as file:
     fejlec=file.readline().strip().split(";")
     for elem in file:
         sor=elem.strip().split()
         if sor[0] not in morzeabc:
             morzeabc[sor[0]]=sor[1]
 
-with open("morze.txt","r") as bemenet:
+with open("morze.txt","r",encoding="ISO-8859-1") as bemenet:
     for i in bemenet:
-        szerzo_idezet=i.strip().split(";")
-        szokoz=i.strip().split("       ")
-
-    def sztring_morzekodda(sztring):
-        uj_sztring=""
-        for elem in sztring:
-            if elem.upper()==" ":
-                uj_sztring+="       "
-            for key,value in morzeabc.items():
-                if elem.upper()==key:
-                    uj_sztring+=value+" "
-        print (f"A megadott sztring morze kódja: {uj_sztring}")
-
-    def morze_dekodolo(kod):
-        uj_sztring=""
-        kod=kod.split()
-        for karakter in kod:
-            for key,value in morzeabc.items():
-                if karakter==value:
-                    uj_sztring+=key+" "
-        print (uj_sztring)
+        morze_dekodolo_2(i)
 
 
-sztring_morzekodda("szia mizu van veled")
-morze_dekodolo(".-   .-.   ..   ...   --..   -   ---   -   .   .-..   ..-..   ...   --..   ")
-print ("+")  
